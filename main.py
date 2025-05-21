ae5f0da453510f2e78aa5e7be2ed614a10d20149
bernikr <git@berni.kr>
2025-05-21 20:12:38 +0200

    allow hiding of overlapping events

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ class SourceConfig(BaseModel):
     url: str
     include: list[str] = []
     event_name: str = "Busy"
+    hide_if_overlapped: bool = False
 
 
 class CalendarConfig(BaseModel):
@@ -54,31 +55,41 @@ def get_ical(cal: str, key: str = "") -> Response:
     keys = CONFIG.calendars[cal].key
     if keys is not None and key not in keys:
         return Response("Unauthorized", status_code=401)
-    c = Calendar()
-    for e in get_events(CONFIG.calendars[cal]):
-        c.add_component(e)
+    c = get_calendar(CONFIG.calendars[cal])
     return Response(c.to_ical(), media_type="text/calendar")
 
 
-def get_events(config: CalendarConfig) -> list[Event]:
-    events = []
+def get_calendar(config: CalendarConfig) -> Calendar:
+    c = Calendar()
     for source in config.sources:
         ical = requests.get(source.url, timeout=5)
         cal = Calendar().from_ical(ical.text)
-        source_events = recurring_ical_events.of(cal).between(
+        source_events: list[Event] = recurring_ical_events.of(cal).between(
             datetime.datetime.now(tz=TZ).date(),
             datetime.datetime.now(tz=TZ).date() + datetime.timedelta(days=config.days_ahead),
-        )
+        )  # type: ignore
         for e in source_events:
-            if e.get("TRANSP", "OPAQUE") == "OPAQUE":
-                ne = Event()
-                for k in ["DTSTART", "DTEND", "DURATION", *source.include]:
-                    if k in e:
-                        ne.add(k, e[k])
-                if "SUMMARY" not in ne:
-                    ne.add("SUMMARY", source.event_name)
-                events.append(ne)
-    return events
+            if e.get("TRANSP", "OPAQUE") != "OPAQUE":
+                continue
+
+            if source.hide_if_overlapped:
+                overlaps: list[Event] = recurring_ical_events.of(c).between(
+                    e.start,
+                    e.end,
+                )  # type: ignore
+                if any(e2.start < e.start and e2.end > e.end for e2 in overlaps):
+                    continue
+
+            ne = Event()
+            ne.start = e.start
+            ne.end = e.end
+            for k in source.include:
+                if k in e:
+                    ne.add(k, e[k])
+            if "SUMMARY" not in ne:
+                ne.add("SUMMARY", source.event_name)
+            c.add_component(ne)
+    return c
 
 
 if __name__ == "__main__":
