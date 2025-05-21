02c959e27eb1ad15d59e4df4370f6572e2baa631
bernikr <git@berni.kr>
2025-05-21 19:34:38 +0200

    uvicorn options for auto reload during deebugging

--- main.py
+++ main.py
@@ -86,4 +86,4 @@ def get_events(config: CalendarConfig) -> list[Event]:
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")  # noqa: S104
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info", reload=True)  # noqa: S104
