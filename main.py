8c0451b3b2c66c045870efb523289a431760ba0a
bernikr <git@berni.kr>
2025-06-03 17:19:02 +0200

    Bump version to 0.4.1

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from fastapi import FastAPI, Response
 from icalendar import Calendar, Event
 from pydantic import BaseModel, BeforeValidator
 
-VERSION = "0.4.0"
+VERSION = "0.4.1"
 
 TZ = pytz.timezone(os.getenv("TZ", "Europe/Vienna"))
 CONFIG_FILE = Path(os.getenv("CONFIG_FILE", "config.yaml"))
