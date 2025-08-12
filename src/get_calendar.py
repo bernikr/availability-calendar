4c9f3f166e6e2a5205a74254d449a2fc59eeb407
bernikr <git@berni.kr>
2025-08-12 15:19:21 +0200

    refactor: extract events_between function

--- src/get_calendar.py
+++ src/get_calendar.py
@@ -48,6 +48,10 @@ def is_all_day(e: Event) -> bool:
     return not isinstance(e.start, datetime.datetime)
 
 
+def events_between(cal: Component, start: datetime.date, end: datetime.date) -> list[Event]:
+    return [e for e in recurring_ical_events.of(cal).between(start, end) if isinstance(e, Event)]
+
+
 @cached(cache=TTLCache(maxsize=10, ttl=15 * 60))
 async def get_calendar(config: CalendarConfig) -> Calendar:
     c = Calendar()
@@ -58,11 +62,12 @@ async def get_calendar(config: CalendarConfig) -> Calendar:
     events: list[tuple[Event, SourceConfig]] = []
     for ical, source in zip(icals, config.sources, strict=True):
         cal = Calendar().from_ical(ical)
-        source_events: list[Component] = recurring_ical_events.of(cal).between(
-            datetime.datetime.now(tz=TZ).date(),
-            datetime.datetime.now(tz=TZ).date() + datetime.timedelta(days=config.days_ahead),
+        source_events = events_between(
+            cal,
+            datetime.datetime.now(tz=TZ),
+            datetime.datetime.now(tz=TZ) + datetime.timedelta(days=config.days_ahead),
         )
-        events.extend((e, source) for e in source_events if isinstance(e, Event))
+        events.extend((e, source) for e in source_events)
 
     for e, source in sorted(events, key=lambda e: (as_datetime(e[0].start), -e[0].duration.total_seconds())):
         if source.filter.name_regex and not re.search(source.filter.name_regex, e.get("SUMMARY", "")):
@@ -72,15 +77,8 @@ async def get_calendar(config: CalendarConfig) -> Calendar:
             continue
 
         if source.hide_if_overlapped and not is_all_day(e):
-            overlaps: list[Component] = recurring_ical_events.of(c).between(
-                e.start,
-                e.end,
-            )
-            if any(
-                e2.start <= e.start and e2.end >= e.end
-                for e2 in overlaps
-                if isinstance(e2, Event) and not is_all_day(e2)
-            ):
+            overlaps = events_between(c, e.start, e.end)
+            if any(e2.start <= e.start and e2.end >= e.end for e2 in overlaps if not is_all_day(e2)):
                 continue
 
         c.add_component(create_event(e, source))
